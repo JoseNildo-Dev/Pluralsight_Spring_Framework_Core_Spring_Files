20b181b34ec912fa3c92fed2ece44cb884fe4cae
José Nildo de Macedo Pereira <jnmpdev@gmail.com>
2022-02-26 15:01:14 -0300

    Feat: Test Scopes
    Testing Singleton and Prototype Scopes

--- 01 - Spring Framework - Spring Fundamentals/conference-java/src/main/java/AppConfig.java
+++ 01 - Spring Framework - Spring Fundamentals/conference-java/src/main/java/AppConfig.java
@@ -2,13 +2,16 @@ import com.pluralsight.conference.repository.HibernateSpeakerRepositoryImpl;
 import com.pluralsight.conference.repository.SpeakerRepository;
 import com.pluralsight.conference.service.SpeakerService;
 import com.pluralsight.conference.service.SpeakerServiceImpl;
+import org.springframework.beans.factory.config.BeanDefinition;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Scope;
 
 @Configuration
 public class AppConfig {
 
     @Bean(name = "speakerService")
+    @Scope(value= BeanDefinition.SCOPE_SINGLETON)
     public SpeakerService getSpeakerService() {
         SpeakerServiceImpl service = new SpeakerServiceImpl(getSpeakerRepository());
         //service.setRepository(getSpeakerRepository());
