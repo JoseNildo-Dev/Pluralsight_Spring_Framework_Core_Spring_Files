c38845f55f9f3e58d937eff34b5a9df444244f39
José Nildo de Macedo Pereira <jnmpdev@gmail.com>
2022-02-26 14:34:14 -0300

    Feat: Configure a setter injection
    Created a setter injection and everything else needed for everything to work

--- 01 - Spring Framework - Spring Fundamentals/conference-java/src/main/java/AppConfig.java
+++ 01 - Spring Framework - Spring Fundamentals/conference-java/src/main/java/AppConfig.java
@@ -1,3 +1,5 @@
+import com.pluralsight.conference.repository.HibernateSpeakerRepositoryImpl;
+import com.pluralsight.conference.repository.SpeakerRepository;
 import com.pluralsight.conference.service.SpeakerService;
 import com.pluralsight.conference.service.SpeakerServiceImpl;
 import org.springframework.context.annotation.Bean;
@@ -8,6 +10,13 @@ public class AppConfig {
 
     @Bean(name = "speakerService")
     public SpeakerService getSpeakerService() {
-        return new SpeakerServiceImpl();
+        SpeakerServiceImpl service = new SpeakerServiceImpl();
+        service.setRepository(getSpeakerRepository());
+        return service;
+    }
+
+    @Bean(name = "speakerRepository")
+    public SpeakerRepository getSpeakerRepository() {
+        return new HibernateSpeakerRepositoryImpl();
     }
 }
